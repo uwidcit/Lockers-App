c6d429d8034ba3daca03569e1b62ab1122ab24bf
TheGreatDevourer <anthonygould2304@outlook.com>
2022-10-18 19:42:43 -0400

    fixed Locker and Area

--- models/area.py
+++ models/area.py
@@ -3,7 +3,7 @@ from database import db
 class Area(db.Model):
     id = db.Column(db.Integer, primary_key =True)
     location = db.Column(db.String, nullable = False)
-    lockers = db.relationship('Locker', backref='area', lazy=True, cascade="all, delete-orphan")
+    lockers = db.relationship('Locker', backref='areas', lazy=True, cascade="all, delete-orphan")
 
 
     def __init__(self,location):
