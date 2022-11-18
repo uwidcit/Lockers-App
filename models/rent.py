e4bf5c5eb52f0b55f306b7f5cb67fc187faddd66
TheGreatDevourer <anthonygould2304@outlook.com>
2022-11-18 02:14:33 +0000

    updated models

--- models/rent.py
+++ models/rent.py
@@ -4,7 +4,7 @@ class Rent(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     student_id = db.Column (db.Integer, db.ForeignKey("student.student_id"), nullable= False)
     locker_id = db.Column(db.String, db.ForeignKey("locker.locker_code"), nullable= False)
-    rent_type =  db.Column(db.Integer, db.ForeignKey("RentType.id"), nullable= False)
+    rent_type =  db.Column(db.Integer, db.ForeignKey("rentType.id"), nullable= False)
     rent_date_from =  db.Column(db.Date, nullable= False)
     rent_date_to = db.Column(db.Date, nullable= False)
     date_returned = db.Column(db.Date, nullable = True)
--- models/rentType.py
+++ models/rentType.py
@@ -1,6 +1,7 @@
 from database import db
 
 class RentType(db.Model):
+    __tablename__ = "rentType"
     id = db.Column(db.Integer, primary_key = True)
     period = db.Column(db.String, nullable = False)
     type = db.Column(db.String, nullable = False)
