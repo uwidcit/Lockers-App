7046dedb928ce3b872ce0330f6d34e5ee142eed4
adonispaul722 <adonispaul7232@gmail.com>
2022-12-18 02:13:32 +0000

    update

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import InputRequired, EqualTo, Email
 
 class Add(FlaskForm):
     locker_code = StringField('locker_code', validators=[InputRequired()])
-    area = RadioField(u'area', choices=[('SAC', 'SAC'), ('ENG', 'Engineer'), ('FST', 'FST'), ('FSS', 'FSS')])
+    area = SelectField(u'area', choices=[('SAC', 'SAC'), ('ENG', 'Engineer'), ('FST', 'FST'), ('FSS', 'FSS')])
     locker_type  = SelectField(u'locker_type', choices=[('Small', 'Small'), ('Medium', 'Medium'), ('Combination', 'Combination')])
     status  = SelectField(u'Programming Language', choices=[('Rented', 'Rented'), ('Repair', 'Repair'), ('Free', 'Free')])
     key_id = StringField('key_id', validators=[InputRequired()])
