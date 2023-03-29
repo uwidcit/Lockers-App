639b858c0bfee15acf4ffc621540c9936c01095c
adonispaul722 <adonispaul7232@gmail.com>
2023-03-29 00:31:04 +0000

    report views updated

--- views/report.py
+++ views/report.py
@@ -2,6 +2,16 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 from controllers import get_current_user
 report_views = Blueprint('report_views', __name__, template_folder='../templates')
 
+from controllers import (
+    get_all_lockers,
+
+
+)
+
 @report_views.route('/report', methods=['GET'])
+def report_page():
+    return render_template('report.html')
+
+@report_views.route('/report/transactions', methods=['POST'])
 def report_page():
     return render_template('report.html')
