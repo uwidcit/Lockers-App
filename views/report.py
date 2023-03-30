cc7a3acaa9becda9bbabd97a84fce2fd48c5b710
adonispaul722 <adonispaul7232@gmail.com>
2023-03-30 02:04:24 +0000

    update

--- views/report.py
+++ views/report.py
@@ -16,11 +16,10 @@ def report_page():
 @report_views.route('/report/transactions', methods=['GET'])
 def transaction_report():
     transaction_data=get_all_transactions()
-    transaction_data_jsonified=jsonify(transaction_data)
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('Arial', '', 16)
-    for log in range(transaction_data_jsonified):
+    for log in range(transaction_data):
         pdf.cell(w=0, h=10, txt=log, ln=1, align='L')
     pdf.output(f'./transaction_report.pdf', 'F')
     flash("success")
