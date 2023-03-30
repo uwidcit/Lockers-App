64a8ea373f34b49bebf17dea289fdea09e3f65a5
adonispaul722 <adonispaul7232@gmail.com>
2023-03-30 02:11:47 +0000

    key and locker report generated

--- views/report.py
+++ views/report.py
@@ -6,7 +6,7 @@ from fpdf import FPDF
 from controllers import (
     get_all_lockers,
     get_all_transactions,
-
+    get_all_keys,
 )
 
 @report_views.route('/report', methods=['GET'])
@@ -14,12 +14,36 @@ def report_page():
     return render_template('report.html')
 
 @report_views.route('/report/transactions', methods=['GET'])
-def transaction_report():
+def transactions_report():
     transaction_data=get_all_transactions()
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('Arial', '', 16)
-    for log in transaction_data:
+    for log in transactions_data:
+        pdf.cell(w=0, h=10, txt=log, ln=1, align='L')
+    pdf.output(f'./transaction_report.pdf', 'F')
+    flash("success")
+    return render_template('report.html')
+
+@report_views.route('/report/lockers', methods=['GET'])
+def lockers_report():
+    lockers_data=get_all_lockers()
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', '', 16)
+    for log in lockers_data:
+        pdf.cell(w=0, h=10, txt=log, ln=1, align='L')
+    pdf.output(f'./transaction_report.pdf', 'F')
+    flash("success")
+    return render_template('report.html')
+
+@report_views.route('/report/keys', methods=['GET'])
+def keys_report():
+    keys_data=get_all_keys()
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', '', 16)
+    for log in keys_data:
         pdf.cell(w=0, h=10, txt=log, ln=1, align='L')
     pdf.output(f'./transaction_report.pdf', 'F')
     flash("success")
