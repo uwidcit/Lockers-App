154e652c868daf34d7a95246a6d90380f7043017
Nicholas Mendez <snickdx@gmail.com>
2023-07-03 14:24:44 +0000

    offline testing

--- sw-src.js
+++ sw-src.js
@@ -7,21 +7,43 @@ importScripts(
 const {registerRoute} = workbox.routing;
 const {CacheFirst} = workbox.strategies;
 const {precacheAndRoute} = workbox.precaching;
-const {CacheableResponse} = workbox.cacheableResponse;
+const {CacheableResponse, CacheableResponsePlugin} = workbox.cacheableResponse;
 
 
-precacheAndRoute(self.__WB_MANIFEST);
 
+precacheAndRoute([ { url: '/locker/offline', revision: null }, ...self.__WB_MANIFEST]);
 
-// runtime data caching
-registerRoute(
-  ({ url }) => url.pathname.startsWith('/locker'),
+// Custom wrapper function to log requests and register the route
+function registerRouteWithLogging(urlPattern, strategy, options) {
+  console.log('Registering route:', urlPattern);
+
+  // Log the request before registering the route
+  self.addEventListener('fetch', (event) => {
+    console.log('Fetching:', event.request.url);
+    // You can log other request details here if needed
+  });
+
+  // Register the route with the provided strategy and options
+  registerRoute(urlPattern, strategy, options);
+}
+
+registerRouteWithLogging(
+  '*', // URL pattern to match
   new CacheFirst({
-    cacheName: 'api',
+    cacheName: 'lockers-cache',
     plugins: [
-      new CacheableResponse({
-        statuses: [200],
-      }),
-    ],
+      new CacheableResponsePlugin({
+        statuses: [0, 200]
+      })
+    ]
   })
 );
+
+// Log all network requests
+self.addEventListener('fetch', (event) => {
+  console.log('SW: Fetching', event.request.url);
+  // You can also log other request details like event.request.method, event.request.headers, etc.
+
+  // Pass through the fetch request
+  event.respondWith(fetch(event.request));
+});
