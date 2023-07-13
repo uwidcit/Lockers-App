f1453212404ad84859728ba523aa258c22d63e01
TheGreatDevourer <anthonygould2304@outlook.com>
2023-07-13 14:25:45 -0400

    updated source service worker

--- sw-src.js
+++ sw-src.js
@@ -4,10 +4,13 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-const {registerRoute} = workbox.routing;
-const {CacheFirst} = workbox.strategies;
+const {offlineFallback} = workbox.recipes;
+const {BackgroundSyncPlugin,Queue} = workbox.backgroundSync;
+const {registerRoute,setDefaultHandler} = workbox.routing;
+const {CacheFirst, StaleWhileRevalidate, NetworkOnly,NetworkFirst} = workbox.strategies;
 const {precacheAndRoute} = workbox.precaching;
 const {CacheableResponse, CacheableResponsePlugin} = workbox.cacheableResponse;
+
 
 
 
@@ -47,3 +50,79 @@ self.addEventListener('fetch', (event) => {
   // Pass through the fetch request
   event.respondWith(fetch(event.request));
 });
+
+const bgSyncPlugin = new BackgroundSyncPlugin('LockersQueue',{
+  maxRetentionTime:48 *60,
+  onSync: customReplay,
+  forceSyncFallback:true,
+
+})
+
+async function customReplay(){
+  let entry;
+  while ((entry = await this.shiftRequest())){
+    try{
+      const response = await fetch(entry.request.clone())
+    }
+    catch (error){
+      await this.unshiftRequest(entry)
+      throw new Error ('Cannot connect to server')
+    }
+  }
+}
+
+const statusPlugin = {
+  fetchDidSucceed: ({response}) =>{
+    console.log(response)
+    if (response.status >= 500){
+      throw new Error('Server error.')
+    }
+    return response
+  }
+}
+
+registerRoute(
+  new RegExp('\/api/*'),
+  new NetworkOnly({
+    plugins:[
+      statusPlugin,
+      bgSyncPlugin,
+    ]
+  }),
+  'POST'
+  )
+  
+registerRoute(
+  new RegExp('\/api/*'),
+  new StaleWhileRevalidate()
+);
+
+registerRoute(
+  new RegExp('\/static/*'),
+  new StaleWhileRevalidate()
+);
+
+registerRoute(
+  new RegExp('\/locker'),
+  new NetworkFirst()
+);
+
+registerRoute(
+  new RegExp('https://code.jquery.com/jquery-3.7.0.min.js'),
+  new CacheFirst()
+);
+
+registerRoute(
+  new RegExp('https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js'),
+  new CacheFirst()
+);
+
+registerRoute(
+  new RegExp('https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js'),
+  new CacheFirst()
+);
+
+registerRoute(
+  new RegExp('https://cdn.datatables.net/select/1.6.2/js/dataTables.select.min.js'),
+  new CacheFirst()
+);
