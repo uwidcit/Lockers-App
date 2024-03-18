c9189184b526e837045f6521c8d4682c6e642ac8
TheGreatDevourer <anthonygould2304@outlook.com>
2024-03-18 15:03:56 -0400

    updated service worker

--- App/static/lockers.js
+++ App/static/lockers.js
@@ -170,7 +170,7 @@ var row_id = 0
 
 async function getLockers(){
     let result = await sendRequest('/api/locker','GET')
-    for (r in result){
+    for (let r in result){
         locker_list.push(result[r])
     }
     initTable(locker_list)
